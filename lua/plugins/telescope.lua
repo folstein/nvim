f8ef9f119b880eb43cce9ea4f11bc5a7b2398446
Steven Folstein <steve@geodetic.com>
2023-09-07 21:41:44 -0400

    More playing around.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,12 +1,4 @@
--- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
---
--- In your plugin files, you can:
--- * add extra plugins
--- * disable/enabled LazyVim plugins
--- * override the configuration of LazyVim plugins
 return {
-
-  -- add telescope-fzf-native
   {
     "telescope.nvim",
     dependencies = {

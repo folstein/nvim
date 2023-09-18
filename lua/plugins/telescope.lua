ab8f9fc3f71e414afe3e661ddada2031167f5ce9
Ubuntu <steve@GitLab-CE.tex3in2m4mguvidkwmyousveta.bx.internal.cloudapp.net>
2023-09-18 21:34:04 +0000

    Compatible with linux and windows.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,9 +1,13 @@
-return {
-  {
-    "telescope.nvim",
-    dependencies = {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+if vim.loop.os_uname().sysname == "Windows_NT" then
+  return {
+    {
+      "telescope.nvim",
+      dependencies = {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+      },
     },
-  },
-}
+  }
+else
+  return {}
+end
